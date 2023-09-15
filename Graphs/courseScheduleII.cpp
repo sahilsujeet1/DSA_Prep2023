90de738143fab9449f38c37056ddf1f1d2303525
Sahil Kumar <sahil.kumar@quantiphi.com>
2023-09-15 13:11:01 +0530

    courseScheduleII

--- Graphs/courseScheduleII.cpp
+++ Graphs/courseScheduleII.cpp
@@ -1,3 +1,45 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+vector<int> topoSortBFS(map<int, list<int>>& adj, int n) {
+    vector<int> ans;
+    unordered_map<int, int> indegree;
+    for(auto node:adj) {
+        for(int nbr:node.second) {
+            indegree[nbr]++;
+        }
+    }
+
+    queue<int> q;
+    for(int i=0; i<n; i++) {
+        if(indegree[i] == 0) 
+            q.push(i);
+    }
+
+    while(!q.empty()) {
+        int front = q.front();
+        q.pop();
+        ans.push_back(front);
+
+        for(int nbr:adj[front]) {
+            indegree[nbr]--;
+            if(indegree[nbr] == 0) {
+                q.push(nbr);
+            }
+        }
+    }
+
+    if(ans.size() == n) return ans;
+    else return {};
+}
+
+vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+    map<int, list<int>> adj;
+
+    for(auto i: prerequisites) {
+        int u = i[1], v = i[0];
+        adj[u].push_back(v);
+    }
+
+    return topoSortBFS(adj, numCourses);
+}
