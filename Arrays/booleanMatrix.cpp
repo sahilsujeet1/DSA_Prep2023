b7d3721eda4fe6892a96166dc03c4a3ca1630e9b
Sahil Kumar <sahilsujeet1@gmail.com>
2023-10-01 13:40:42 +0530

    common in three sorted arrays

---
+++ Arrays/booleanMatrix.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+void convert(int arr[4][3]) {
+    for(int i=0; i<4; i++) {
+        for(int j=0; i<3; j++) {
+            if(arr[i][j] == 1) {
+                
+                
+            }
+        }
+    }
+}
+
+int main() {
+    int arr[4][3] = {
+        {1,0,0},
+        {1,0,0},
+        {1,0,0},
+        {0,0,0}
+    };
+
+    convert(arr);
+}
