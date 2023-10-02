807e063edd58973273851186211e0ef1a536e217
Sahil Kumar <sahilsujeet1@gmail.com>
2023-10-02 15:22:47 +0530

    factorial of large numbers

---
+++ Arrays/largeFactorial.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> factorial(int N){
+    // code here
+    vector<int> ans;
+    ans.push_back(1);
+    int carry = 0;
+    
+    for(int i=2; i<=N; i++) {
+        for(int j=0; j<ans.size(); j++) {
+            int x = ans[j] * i + carry;
+            ans[j] = x%10;
+            carry = x/10;
+        }
+        
+        while(carry) {
+            ans.push_back(carry%10);
+            carry /= 10;
+        }
+        
+        carry = 0;
+    }
+    
+    reverse(ans.begin(), ans.end());
+    return ans;
+}
