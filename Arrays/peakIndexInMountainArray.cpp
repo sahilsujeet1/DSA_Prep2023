66e5369302b12cd6ced6cfabb87b977c79a20774
Sahil Kumar <sahilsujeet1@gmail.com>
2023-10-03 16:05:55 +0530

    peak index in mountain array

---
+++ Arrays/peakIndexInMountainArray.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int peakIndexInMountainArray(vector<int>& arr) {
+    int s=0, e = arr.size()-1;
+
+    while(s < e) {
+        int mid = s + (e-s)/2;
+
+        if(arr[mid] < arr[mid+1]) {
+            s = mid+1;
+        } else {
+            e = mid;
+        }
+    }
+
+    return e;
+}
