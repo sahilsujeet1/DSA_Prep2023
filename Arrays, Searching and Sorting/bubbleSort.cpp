a3c26d2106e1b218e2fbc5ba2bde2a51b2d1fbb6
Sahil Kumar <sahilsujeet1@gmail.com>
2023-10-07 21:55:25 +0530

    bubble sort

---
+++ Arrays, Searching and Sorting/bubbleSort.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+// sort ith largest element in each round
+void bubbleSort(int *arr, int n) {
+    for(int i=1; i<n; i++) {
+        for(int j=0; j<n-i; j++){
+            if(arr[j] > arr[j+1]) {
+                swap(arr[j], arr[j+1]);
+            }
+        }
+    }
+}
+
+int main() {
+    int arr[5] = {4,2,56,1,3};
+    bubbleSort(arr, 5);
+
+    for(int i=0; i<5; i++) {
+        cout << arr[i] << ", ";
+    }
+}
