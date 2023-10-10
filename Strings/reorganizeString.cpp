f9e4756e12d0eac5ed9e4c0952f2251c496e9cc1
Sahil Kumar <sahilsujeet1@gmail.com>
2023-10-10 18:59:58 +0530

    reorganize string

---
+++ Strings/reorganizeString.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string reorganizeString(string s) {
+        int hash[26] = {0};
+        for(int i=0; i<s.length(); i++) hash[s[i]-'a']++;
+
+        char mostFreqChar;
+        int maxFreq = INT_MIN;
+
+        for(int i=0; i<26; i++) {
+            if(hash[i] > maxFreq) {
+                maxFreq = hash[i];
+                mostFreqChar = i + 'a';
+            }
+        }
+
+        int index = 0;
+        while(maxFreq > 0 && index < s.length()) {
+            s[index] = mostFreqChar;
+            maxFreq--;
+            index += 2;
+        }
+
+        if(maxFreq != 0) return "";
+
+        hash[mostFreqChar - 'a'] = 0;
+
+        for(int i=0; i<26; i++) {
+            while(hash[i] > 0) {
+                index = index >= s.length() ? 1 : index;
+                s[index] = i + 'a';
+                hash[i]--;
+                index += 2;
+            }
+        }
+
+        return s;
+    }
+};
