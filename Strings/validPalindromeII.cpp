5aa782037edc447fa7a1d261e400053a1e019a74
Sahil Kumar <sahilsujeet1@gmail.com>
2023-10-10 13:06:54 +0530

    valid palindrome II

--- Strings/validPalindromeII.cpp
+++ Strings/validPalindromeII.cpp
@@ -1,3 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+class Solution {
+public:
+    bool isPalindrome(string s, int i, int e) {
+        while(i<=e) {
+            if(s[i++] != s[e--])
+                return false;
+        }
+        return true;
+    }
+
+    bool validPalindrome(string s) {
+        int i=0, j=s.length()-1;
+
+        while(i < j) {
+            if(s[i] != s[j]) {
+                return (isPalindrome(s, i+1, j) || isPalindrome(s, i, j-1));
+            } else {
+                i++; j--;
+            }
+        }
+
+        return true;
+    }
+};
