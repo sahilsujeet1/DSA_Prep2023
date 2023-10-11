774b9cbb44d16de3ae02e7db0fbf3e98eac38c1a
Sahil Kumar <sahilsujeet1@gmail.com>
2023-10-11 21:05:12 +0530

    first occurrence index of string B in string A

---
+++ Strings/firstIndexOfString.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int n = haystack.length();
+        int m = needle.length();
+
+        for(int i=0; i<=n-m; i++) {
+            for(int j=0; j<m; j++) {
+                if(needle[j] != haystack[i+j]) {
+                    break;
+                }
+
+                if(j == m-1) return i;
+            }
+        }
+
+        return -1;
+    }
+};
